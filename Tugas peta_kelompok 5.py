d54b5869b0fb8d6d3ac3f56e59b02617c12657f8
Fauzi162 <147597558+Fauzi162@users.noreply.github.com>
2024-05-02 20:07:22 +0700

    Update Tugas peta_kelompok 5.py

--- Tugas peta_kelompok 5.py
+++ Tugas peta_kelompok 5.py
@@ -23,7 +23,31 @@ class Peta:
             del self.daftarKota[kotaDihapus]
     
     def hapusJarak(self, kota1, kota2):
+    if kota1 and kota2 in self.daftarKota:
+            del self.daftarKota[kota1][kota2]
+            del self.daftarKota[kota2][kota1]
 
+petaJepang = Peta()
+petaJepang.tambahKota("Mori")
+petaJepang.tambahKota("Iwata")
+petaJepang.tambahKota("Fukuroi")
+petaJepang.tambahKota("Kakegawa")
+petaJepang.tambahKota("Kikugawa")
+petaJepang.tambahKota("Shimada")
+petaJepang.tambahKota("Fujieda")
+petaJepang.tambahKota("Yaizu")
+petaJepang.tambahKota("Yoshida")
+petaJepang.tambahKota("Makinohara")
+petaJepang.tambahKota("Omaezaki")
+
+petaJepang.tambahJarak("Mori", "Iwata", 19)
+petaJepang.tambahJarak("Mori", "Fukuroi", 12)
+petaJepang.tambahJarak("Mori", "Kakegawa", 14)
+petaJepang.tambahJarak("Mori", "Kikugawa", 23)
+petaJepang.tambahJarak("Mori", "Shimada", 30)
+petaJepang.tambahJarak("Mori", "Fujieda", 40)
+petaJepang.tambahJarak("Iwata", "Fukuroi", 9)
+petaJepang.tambahJarak("Iwata", "Omaezaki", 30)
 
 
 
